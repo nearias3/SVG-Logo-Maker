c7801495699b38ca912b6785d08d41730cd5f50c
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-22 16:13:31 -0500

    Got code finished, troubleshooting error with prompt function

--- index.js
+++ index.js
@@ -2,39 +2,53 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Triangle, Circle, Square } = require('./lib/shapes');
 
-function promptQuestions() {
-inquirer.prompt([{
+function promptUser() {
+inquirer.prompt([
+    {
     type: 'input',
     name:'text',
     message: 'Enter the text you want on the logo (up to 3 characters):',
-    // how do I validate user input to ensure they don't put more than 3? //
-},
-{
+    },
+    {
     type: 'input',
     name: 'textColor',
     message: 'Enter the text color (keyword or hexadecimal):',
-},
-{
+    },
+    {
     type: 'list',
     name: 'shape',
     message: 'Choose a shape for the logo:',
-    choices: [circle, triangle, square],
-},
-{
+    choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
     type: 'input',
     name: 'shapeColor',
     message: 'Enter the shape color (keyword or hexadecimal):',
-},
+    },
 ])
     .then(answers => {
         if (answers.text.length > 3) {
             console.log("Value must not exceed 3 characters");
             promptQuestions();
         } else {
-            fs.writeToFile('logo.svg', answers);
+            let shape;
+            switch (answers.shape) {
+                case 'Circle':
+                    shape = new Circle();
+                    break;
+                case 'Triangle':
+                    shape = new Triangle();
+                    break;
+                case 'Square':
+                    shape = new Square();
+                    break;
+            }
+            shape.setColor(answers.shapeColor);
+            
+            fs.writeFileSync('logo.svg', answers);
             console.log('Your logo.svg was successfully generated!')
         }
         });
 }
 
-promptQuestions();
+promptUser();
